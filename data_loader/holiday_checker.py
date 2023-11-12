3937f6ad2e7f0b69c284337388a5c565fcabdaba
DevSmapy <lyk456@naver.com>
2023-11-12 14:10:38 +0900

    feat: Add holiday_checker.py

---
+++ data_loader/holiday_checker.py
@@ -0,0 +1,27 @@
+from datetime import date
+from typing import Optional
+
+from data_loader import data_to_dict, generate_url_params, get_data
+
+
+def is_weekend(input_date: str) -> bool:
+    return input_date.weekday() > 4
+
+
+def convert_to_date(date_str: str) -> date:
+    return date(int(date_str[:4]), int(date_str[4:6]), int(date_str[6:]))
+
+
+def get_holiday_info(data: dict) -> Optional[list]:
+    try:
+        return data["response"]["body"]["items"]["item"]
+    except (KeyError, TypeError):
+        return
+
+
+def get_holiday_date(data: list) -> Optional[set]:
+    return [holiday["locdate"] for holiday in data]
+
+
+def check_holiday(holidays: list, input_date: str) -> bool:
+    return input_date in holidays
