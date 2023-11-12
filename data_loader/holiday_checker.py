4aaa866a48873ff6bb96a82ef4a8a10e20281e38
DevSmapy <lyk456@naver.com>
2023-11-12 14:48:24 +0900

    fix: Change input, output type annotation

--- data_loader/holiday_checker.py
+++ data_loader/holiday_checker.py
@@ -1,8 +1,8 @@
 from datetime import date
-from typing import Optional
+from typing import Any, Dict, List, Optional
 
 
-def is_weekend(input_date: str) -> bool:
+def is_weekend(input_date: date) -> bool:
     return input_date.weekday() > 4
 
 
@@ -10,16 +10,16 @@ def convert_to_date(date_str: str) -> date:
     return date(int(date_str[:4]), int(date_str[4:6]), int(date_str[6:]))
 
 
-def get_holiday_info(data: dict) -> Optional[list]:
+def get_holiday_info(data: Dict[str, Any]) -> Optional[List[Dict[str, str]]]:
     try:
         return data["response"]["body"]["items"]["item"]
     except (KeyError, TypeError):
         return
 
 
-def get_holiday_date(data: list) -> Optional[list]:
+def get_holiday_date(data: List[Dict[str, str]]) -> Optional[List[str]]:
     return [holiday["locdate"] for holiday in data]
 
 
-def check_holiday(holidays: list, input_date: str) -> bool:
+def check_holiday(holidays: List[str], input_date: str) -> bool:
     return input_date in holidays
