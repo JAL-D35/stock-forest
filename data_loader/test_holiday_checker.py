92c815bfa19babbee3a7f411590682c853e36dc6
DevSmapy <lyk456@naver.com>
2023-11-19 17:54:46 +0900

    feat: Add `test_convert_to_date` function

--- data_loader/test_holiday_checker.py
+++ data_loader/test_holiday_checker.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date, datetime
 
 import pytest
 
@@ -21,3 +21,19 @@ BASE_DATETIME = datetime(2023, 11, 13)
 )
 def test_is_weekend(input_date: datetime, expected: bool) -> None:
     assert Weekday.is_weekend(input_date) == expected
+
+
+@pytest.mark.parametrize(
+    "input_date, expected",
+    [
+        ("20231113", date(2023, 11, 13)),
+        ("20231114", date(2023, 11, 14)),
+        ("20231115", date(2023, 11, 15)),
+        ("20231116", date(2023, 11, 16)),
+        ("20231117", date(2023, 11, 17)),
+        ("20231118", date(2023, 11, 18)),
+        ("20231119", date(2023, 11, 19)),
+    ],  # yapf: disable
+)
+def test_convert_to_date(input_date: str, expected: datetime) -> None:
+    assert convert_to_date(input_date) == expected
