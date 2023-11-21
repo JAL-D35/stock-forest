28eaf90eb345d928a718ad520f29772fbf06e227
DevSmapy <lyk456@naver.com>
2023-11-21 22:20:16 +0900

    fix: Change output type annotation

--- data_loader/test_holiday_checker.py
+++ data_loader/test_holiday_checker.py
@@ -21,11 +21,11 @@ BASE_DATE = datetime(randint(0, 2023), randint(1, 12), randint(1, 31))
         BASE_DATE + timedelta(days=6),
     ],
 )
-def test_is_weekend(input_date: datetime) -> Optional[bool]:
+def test_is_weekend(input_date: datetime) -> None:
     assert Weekday.is_weekend(input_date) == (input_date.weekday() > 4)  # 4 is Friday
 
 
-def test_convert_to_date() -> Optional[bool]:
+def test_convert_to_date() -> None:
     input_date = BASE_DATE.strftime("%Y%m%d")  # e.g. 20210101
     expected = BASE_DATE.date()
     assert convert_to_date(input_date) == expected
