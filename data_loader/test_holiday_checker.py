1487a11ad52a3f7ad7862507a07ec37476d6483b
DevSmapy <lyk456@naver.com>
2023-11-21 17:29:17 +0900

    fix: Change `test_is_weekend` test function

--- data_loader/test_holiday_checker.py
+++ data_loader/test_holiday_checker.py
@@ -1,26 +1,28 @@
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
+from random import randint
+from typing import Optional
 
 import pytest
 
 from .holiday_checker import Weekday, convert_to_date
 
-BASE_DATETIME = datetime(2023, 11, 13)
+BASE_DATE = datetime(randint(0, 2023), randint(1, 12), randint(1, 31))
 
 
 @pytest.mark.parametrize(
-    "input_date, expected",
+    "input_date",
     [
-        (datetime(2023, 11, 13), False),
-        (datetime(2023, 11, 14), False),
-        (datetime(2023, 11, 15), False),
-        (datetime(2023, 11, 16), False),
-        (datetime(2023, 11, 17), False),
-        (datetime(2023, 11, 18), True),
-        (datetime(2023, 11, 19), True),
+        BASE_DATE,
+        BASE_DATE + timedelta(days=1),
+        BASE_DATE + timedelta(days=2),
+        BASE_DATE + timedelta(days=3),
+        BASE_DATE + timedelta(days=4),
+        BASE_DATE + timedelta(days=5),
+        BASE_DATE + timedelta(days=6),
     ],
 )
-def test_is_weekend(input_date: datetime, expected: bool) -> None:
-    assert Weekday.is_weekend(input_date) == expected
+def test_is_weekend(input_date: datetime) -> Optional[bool]:
+    assert Weekday.is_weekend(input_date) == (input_date.weekday() > 4)  # 4 is Friday
 
 
 @pytest.mark.parametrize(
