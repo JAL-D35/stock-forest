f0f04a75d85dbc057758408146710b9e548401f3
DevSmapy <lyk456@naver.com>
2023-11-22 09:31:58 +0900

    fix: Change variable `BASE_TIME` to `BASE_DATETIME`

--- data_loader/test_holiday_checker.py
+++ data_loader/test_holiday_checker.py
@@ -6,19 +6,19 @@ import pytest
 
 from .holiday_checker import Weekday, convert_to_date
 
-BASE_DATE = datetime(randint(0, 2023), randint(1, 12), randint(1, 31))
+BASE_DATETIME = datetime(randint(0, 2023), randint(1, 12), randint(1, 31))
 
 
 @pytest.mark.parametrize(
     "input_date",
     [
-        BASE_DATE,
-        BASE_DATE + timedelta(days=1),
-        BASE_DATE + timedelta(days=2),
-        BASE_DATE + timedelta(days=3),
-        BASE_DATE + timedelta(days=4),
-        BASE_DATE + timedelta(days=5),
-        BASE_DATE + timedelta(days=6),
+        BASE_DATETIME,
+        BASE_DATETIME + timedelta(days=1),
+        BASE_DATETIME + timedelta(days=2),
+        BASE_DATETIME + timedelta(days=3),
+        BASE_DATETIME + timedelta(days=4),
+        BASE_DATETIME + timedelta(days=5),
+        BASE_DATETIME + timedelta(days=6),
     ],
 )
 def test_is_weekend(input_date: datetime) -> None:
@@ -26,6 +26,6 @@ def test_is_weekend(input_date: datetime) -> None:
 
 
 def test_convert_to_date() -> None:
-    input_date = BASE_DATE.strftime("%Y%m%d")  # e.g. 20210101
-    expected = BASE_DATE.date()
+    input_date = BASE_DATETIME.strftime("%Y%m%d")  # e.g. 20210101
+    expected = BASE_DATETIME.date()
     assert convert_to_date(input_date) == expected
