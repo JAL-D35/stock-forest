0807fb04e97058dc86bd0ada25852c2ae7d23410
Anthony Lee <54012343+DevSmapy@users.noreply.github.com>
2023-12-01 11:16:55 +0900

    fix: Fix `holiday_checker.py` (#13)

--- data_loader/holiday_checker.py
+++ data_loader/holiday_checker.py
@@ -1,6 +1,6 @@
 from datetime import date, datetime
 from enum import IntEnum
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 
 
 class Weekday(IntEnum):
@@ -24,15 +24,29 @@ def convert_to_date(date_str: str) -> date:
     return datetime.strptime(date_str, "%Y%m%d").date()
 
 
-def get_holiday_info(data: Dict[str, Any]) -> Optional[List[Dict[str, str]]]:
+def get_holiday_info(
+    data: Dict[str, Any]
+) -> Optional[Union[List[Dict[str, str]], Dict[str, str]]]:
     try:
         return data["response"]["body"]["items"]["item"]
     except (KeyError, TypeError):
         return
 
 
-def get_holiday_date(data: List[Dict[str, str]]) -> List[str]:
-    return [holiday["locdate"] for holiday in data]
+def check_key_in_dict(input_dict: Dict[str, Any], key: str) -> bool:
+    return key in input_dict
+
+
+def get_holiday_date(
+    data: Union[List[Dict[str, str]], Dict[str, str]]
+) -> Optional[List[str]]:
+    if isinstance(data, dict):
+        if check_key_in_dict(data, "locdate"):
+            return [data["locdate"]]
+        return
+    return [
+        holiday["locdate"] for holiday in data if check_key_in_dict(holiday, "locdate")
+    ]
 
 
 def check_holiday(holidays: List[str], input_date: str) -> bool:
