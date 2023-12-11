0722c5944b41601a68cdbbe00c1e098e6e19d457
DevSmapy <lyk456@naver.com>
2023-12-11 22:11:46 +0900

    fix: Change function name and parameters

--- src/old_data_loader.py
+++ src/old_data_loader.py
@@ -1,21 +1,19 @@
-from datetime import datetime
-
 from main import run
 
 DATE_FORMAT = "%Y%m%d"
 
 
-def save_data_local_until_now(private_key: str, start_date_str: str) -> None:
-    end_date = datetime.now().date()
-
+def save_data_date_range(
+    private_key: str, start_date_str: str, end_date_str: str, output_dir: str
+) -> None:
     run(
         start_date=start_date_str,
-        end_date=end_date.strftime(DATE_FORMAT),
+        end_date=end_date_str,
         private_key=private_key,
         result_type="xml",
         market_class="KOSDAQ",
         n_rows="1700",
         page_no="1",
         output_format="parquet",
-        output_dir=start_date_str,
+        output_dir=output_dir,
     )
